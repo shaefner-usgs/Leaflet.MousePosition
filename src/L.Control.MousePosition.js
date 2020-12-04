7c1bb7d86956d8f94f1e1c7c1b59e00981ad52e1
Scott Haefner <shaefner@usgs.gov>
2020-12-04 12:07:11 -0800

    create a bottom-center control container option

--- src/L.Control.MousePosition.js
+++ src/L.Control.MousePosition.js
@@ -54,6 +54,22 @@ L.Control.MousePosition = L.Control.extend({
 
 });
 
+/**
+ * Extend Map._initControlPos to create a bottom-center control container
+ */
+L.Map.prototype._initControlPos = (function (_initControlPos) {
+  return function () {
+    _initControlPos.apply(this, arguments); // original method
+
+    // Add new control-container
+    this._controlCorners.bottomcenter = L.DomUtil.create(
+      'div',
+      'leaflet-bottom leaflet-center',
+      this._controlContainer
+    );
+  };
+}(L.Map.prototype._initControlPos));
+
 L.Map.mergeOptions({
 	positionControl: false
 });
